689f8cf2428c012dd6aa585c93b6624fd65ac470
Piyush <40433598+knightowl2704@users.noreply.github.com>
2019-05-23 20:08:04 +0530

    Add files via upload

---
+++ Wine_Quality_Testing_SVR.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+dataset_url = 'http://mlr.cs.umass.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+data = pd.read_csv(dataset_url, sep=';')
+ 
+# 4. Split data into training and test sets
+y = data.quality
+X = data.drop('quality', axis=1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 1/3)
+
+from sklearn.preprocessing import StandardScaler
+scX = StandardScaler()
+X_train = scX.fit_transform(X_train)
+X_test = scX.transform(X_test)
+
+
+from sklearn.svm import SVR
+regressor = SVR(kernel="linear")  
+regressor.fit(X_train, y_train)  
+y_pred = regressor.predict(X_test)
+
+from sklearn import metrics
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+
